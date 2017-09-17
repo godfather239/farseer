3fcebd136d7a276b09a759dd56c70ab81a945450
godfather239 <greenday239@sina.com>
2017-09-17 18:53:12 +0800

    add basic spring codes

---
+++ src/main/java/com/farseer/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.farseer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by greenday on 17/09/2017.
+ */
+@Controller
+public class MainController {
+
+    @ResponseBody
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
